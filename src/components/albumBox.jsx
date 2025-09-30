51b84a978a3bef942bb423d0a262abe91be4b1b3
tldms0507 <sieun050783@gmail.com>
2025-09-30 18:29:38 +0900

    FEAT : 추천 앨번 컴포넌트 생성

--- src/components/albumBox.jsx
+++ src/components/albumBox.jsx
@@ -1,7 +1,11 @@
 export default function AlbumBox() {
     return (
-        <div>
-            <h1>Album Box</h1>
+        <div className="w-80 h-20 bg-white flex items-center justify-center rounded-[10px] shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] border border-stone-300">
+            {/* <img src={album} alt="album" className="w-16 h-16" /> */}
+            <div className="flex flex-col items-start justify-center p-4">
+                <p className="text-sm font-bold">Album Title</p>
+                <p className="text-sm font-light">Artist Name</p>
+            </div>
         </div>
     )
 }
--- src/pages/recommend/recommendResult.jsx
+++ src/pages/recommend/recommendResult.jsx
@@ -1,11 +1,11 @@
-
+import AlbumBox from '../../components/albumBox';
 
 export default function RecommendResult() {
 
 
     return (
         <div className="min-h-screen w-full bg-[#EEF5FF] max-w-96 flex flex-col items-center justify-center">
-         
+            <AlbumBox />
         </div>
     )
 }
