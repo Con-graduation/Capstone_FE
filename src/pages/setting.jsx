81f5e1b7bd89cf5a22e2920567b2905510918c2d
tldms0507 <sieun050783@gmail.com>
2025-11-14 17:45:42 +0900

    FEAT : 회원가입 API 연동

--- src/pages/setting.jsx
+++ src/pages/setting.jsx
@@ -3,6 +3,14 @@ import { useNavigate } from 'react-router-dom';
 export default function Setting() {
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('name');
+    localStorage.removeItem('nickname');
+    localStorage.removeItem('level');
+    localStorage.removeItem('googleAuth');
+    navigate('/');
+  };
   return (
     <div className="min-h-screen w-screen bg-[#EEF5FF] flex flex-col">
       <h1 className="text-2xl font-bold pt-10 text-center">설정</h1>
@@ -22,7 +30,9 @@ export default function Setting() {
           </svg>
           알림 설정
         </button>
-        <button className="w-80 py-2 bg-red-400 text-white rounded-md font-bold flex items-center justify-center gap-2 hover:bg-red-500 transition-colors duration-200">
+        <button className="w-80 py-2 bg-red-400 text-white rounded-md font-bold flex items-center justify-center gap-2 hover:bg-red-500 transition-colors duration-200"
+        onClick={handleLogout}
+        >
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
             <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
           </svg>
